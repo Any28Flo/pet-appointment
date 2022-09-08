1f0d9e26119200bfdaf78eec99ffd1ccb21a7462
Analin Flores <foglzerika@gmail.com>
2022-09-08 11:07:49 -0500

    feat | Init project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    return (
+    <div className="App">
+     <div className="container mx-auto">
+         <div className="columns-12 columns-lg-61">
+             <h2>hola</h2>
+         </div>
+         <div className="columns-3 columns-3lg">
+             <h2>form</h2>
+         </div>
+     </div>
+    </div>
+  )
+}
+
+export default App
